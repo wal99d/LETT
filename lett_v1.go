116f5cf9b50ab8259c80fbb06685746d419bcdfc
WaL99D <WaL99D@gmail.com>
2016-03-26 10:31:33 +0300

    Completed EncapsultePushData()

--- lett_v1.go
+++ lett_v1.go
@@ -5,6 +5,47 @@ import(
 	"log"
 )
 
+//LETT Packet consists of [Header : Destination Address: Length of Expected Data : Data it self : Checksum to validate the bytes]
 type LETT struct{
-	//TODO Add Packet Format here
+	Header byte
+	DestinationAddress byte
+    Length [4]byte
+    Data []byte
+    Checksum []byte
 }
+
+type Packet LETT
+type Data []byte
+
+//These are const for Headers type in LETT protocol
+const (
+	PUSH := 0x01
+	PULL := 0x02
+	REPUSH := 0x03
+)
+
+//This function will handle Packet processing from TCP/IP and return *Data
+func (p *Packet) GetData() err, Data{
+	//TODO: Extract DATA from Packet struct and return its address
+}
+
+//This function will encapsulte Data into LETT/Packet
+func (p *Packet) EncapsultePushData(data Data) err {
+	p.Header = data[0]
+	p.DestinationAddress = data[1]
+	p.Length = data[2:6]
+	p.Data = data[5:]
+	//TODO: generate checksum from bytes and store it in p.Checksum, need to be tested once done
+	//p.Checksum = 
+	return nil
+}
+
+//This function must be called first to sort the Data bytes received from Eth before extracting Data 
+func (d Data) BigtoLittleEndian() err{
+	//TODO: Handle conversion from Big 2 Little Endian 
+}
+
+//This function must be called first to sort the Data bytes received from Eth before extracting Data 
+func (d Data) LittletoBigEndian() err{
+	//TODO: Handle conversion from Little 2 Big Endian 	
+}	
