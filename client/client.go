f6a2b7044004366d02f08a6b2bea7e67ff184860
Waleed M ALHarthi <wal99d@gmail.com>
2016-04-21 16:51:41 +0300

    Update client.go

--- client/client.go
+++ client/client.go
@@ -7,21 +7,34 @@ import(
 )
 
 func main(){
-
-	lettproto:= &lett.LETTProtocol{
-		Header: lett.COMMAND,
-		PacketNumber: 1,
-		SensorId: 1,
-		Stats: lett.ON,
-	}
 	
 	conn , err:= net.Dial("tcp", "127.0.0.1:23232")
 	if err!=nil{
 		fmt.Println(err)
 	}
-	//Send new LETT Protocol Packet
-	lett.NewLETTPacket(&conn, lettproto)
-	
+	data:="Thermostat:13C,door:opend"
+	rdng:= []byte(data)
+	var rdngSize int
+	if len(rdng)<=8 {
+		rdngSize= 1
+	}else{
+		rdngSize= len(rdng)/8
+	}
+	for i:=0; i<=rdngSize; i++{
+		lettproto:= &lett.LETTProtocol{
+			Header: lett.READINGS,
+			PacketNumber: uint8(i),
+			SensorId: 1,
+			CollectorId:1,
+			Readings: rdng[i],
+		}
+		//Marshal new LETT Protocol Packet
+		packet:=lettproto.Marshal()
+		conn.Write(packet)
+		fmt.Printf("Sending...Packet %d\n",i)
+	}
+	conn.Close()
+
 	//Readout COMMANDs from LETT Server
 	/*packet, err:=lett.GetLETTProtocolPacket(&conn)
 	if err!=nil{
